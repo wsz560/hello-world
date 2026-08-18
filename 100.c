6a0e4f6c1bde3ea79ba82c7c13b4944a78daaa87
wsz560 <3319429264@qq.com>
2026-08-18 08:48:46 +0800

    添加了找零程序

---
+++ 100.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int price = 0;
+    printf("«Î ‰»Îº€∏Ò:");
+    scanf("%d", &price);
+    int change = 100 - price;
+    printf("’“¡„: %d\n", change);
+    return 0;
+}
